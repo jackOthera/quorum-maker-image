6a224026a7fd6b26454bbe0d31e63f43b1b5d75e
Jack Lever <jack@othera.io>
2018-08-27 17:05:17 +1000

    Version bump of quorum and constellation

--- lib/install_constellation.sh
+++ lib/install_constellation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wget https://github.com/jpmorganchase/constellation/releases/download/v0.3.2/constellation-0.3.2-ubuntu1604.tar.xz
-tar -xf constellation-0.3.2-ubuntu1604.tar.xz
-cp constellation-0.3.2-ubuntu1604/constellation-node /usr/bin/.
+wget https://github.com/jpmorganchase/constellation/releases/download/v0.3.5-build.1/constellation-0.3.5-ubuntu1604.tar.gz
+tar zxvf constellation-0.3.5-ubuntu1604.tar.gz
+cp constellation-node /usr/bin/.
 cd ..
 
 rm -rf go1.10.linux-amd64.tar.gz
